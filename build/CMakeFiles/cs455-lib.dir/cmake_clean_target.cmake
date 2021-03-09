dacbbfdd3ec1f65d73e9a4ce76b9399b46120b39
quan <nguyenquan263@quans-air.main.local.una.edu>
2021-03-09 10:55:09 -0600

    fix swagger problem, add .gitignore

--- build/CMakeFiles/cs455-lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libcs455-lib.a"
-)
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/nguyenquan263/Documents/Development/CS455ProjectGit/CS455Project
-# Build directory: /Users/nguyenquan263/Documents/Development/CS455ProjectGit/CS455Project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(project-tests "cs455-test")
-set_tests_properties(project-tests PROPERTIES  _BACKTRACE_TRIPLES "/Users/nguyenquan263/Documents/Development/CS455ProjectGit/CS455Project/CMakeLists.txt;98;add_test;/Users/nguyenquan263/Documents/Development/CS455ProjectGit/CS455Project/CMakeLists.txt;0;")
